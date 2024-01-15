b2e824cac2583ba3b6c9ffc97e1a734151613d02
Gorthdzx <krislaksana123@gmail.com>
2024-01-15 10:56:17 +0700

    refac : vertical grid images

--- app/src/main/java/app/dito/mytopic/MainActivity.kt
+++ app/src/main/java/app/dito/mytopic/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -18,11 +22,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.dito.mytopic.data.ImageData
 import app.dito.mytopic.models.Topic
 import app.dito.mytopic.ui.theme.MyTopicTheme
@@ -45,9 +51,60 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GridHobby() {
-    GridHobbyList(
-        topicList = ImageData.topics
-    )
+    GridHobbyList(topicsList = ImageData.topics)
+}
+
+@Composable
+fun GridHobbyImageLayout(topic: Topic, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .padding(8.dp)
+
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier.padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(topic.images),
+                contentDescription = stringResource(topic.topicDescription),
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .size(68.dp, 68.dp)
+                    .aspectRatio(1f)
+            )
+
+            Column {
+                Text(
+                    text = LocalContext.current.getString(topic.topicDescription),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontSize = 12.sp
+                )
+                Spacer(
+                    modifier = Modifier.height(5.dp)
+                )
+                Text(
+                    text = topic.topicImages.toString(),
+                    style = MaterialTheme.typography.labelMedium,
+                    fontSize = 10.sp
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun GridHobbyList(topicsList: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2)
+    ) {
+        items(topicsList) { topicList ->
+            GridHobbyImageLayout(
+                topic = topicList, modifier = modifier.padding(8.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -55,45 +112,5 @@ fun GridHobby() {
 fun GridListHobbyPreview() {
     MyTopicTheme {
         GridHobby()
-    }
-}
-
-
-@Composable
-fun GridHobbyImageLayout(topic: Topic, modifier: Modifier = Modifier) {
-    Card {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = modifier.padding(8.dp)
-        ) {
-            Image(
-                painter = painterResource(topic.images),
-                contentDescription = stringResource(topic.topicDescription)
-            )
-
-            Column {
-                Text(
-                    text = LocalContext.current.getString(topic.topicDescription)
-                )
-
-                Text(
-                    text = LocalContext.current.getString(topic.topicImages)
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun GridHobbyList(topicList: List<Topic>, modifier: Modifier = Modifier) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 6.dp)
-    ) {
-        items(topicList) { topicList ->
-            GridHobbyImageLayout(
-                topic = topicList,
-                modifier = modifier.padding(8.dp)
-            )
-        }
     }
 }
